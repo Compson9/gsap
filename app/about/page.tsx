c95a8b8777449ec8d8de367d6f6abc4ec3dd8149
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2024-12-07 11:34:06 +0000

    final commit for learning

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+    <h1 className='text-7xl font-extrabold text-center mt-10'>
+        This is the About Page I beg you go and vote wai
+    </h1>
+    </>
+  )
+}
